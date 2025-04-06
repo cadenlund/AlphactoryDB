a191228c7e69bc1be805242172b26eb1e712abd8
Caden Lund <caden@DESKTOP-MIC3ODI.>
2025-04-06 06:01:27 -0500

    Added fucntion to adjust prices for splits and stored all data in a database

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,11 @@
-CREATE TABLE stock_metadata (
-    ticker TEXT PRIMARY KEY,
-    active BOOLEAN,
-    type TEXT,
-    market_cap NUMERIC,
-    primary_exchange TEXT,
-    sic_description TEXT,
-    list_date DATE,
-    name TEXT,
-    description TEXT,
-    total_employees INTEGER,
-    share_class_shares_outstanding BIGINT,
-    weighted_shares_outstanding BIGINT
+CREATE TABLE IF NOT EXISTS ohlcv_data (
+    time         TIMESTAMPTZ NOT NULL,
+    ticker       TEXT NOT NULL,
+    open         DOUBLE PRECISION NOT NULL,
+    close        DOUBLE PRECISION NOT NULL,
+    high         DOUBLE PRECISION NOT NULL,
+    low          DOUBLE PRECISION NOT NULL,
+    volume       DOUBLE PRECISION NOT NULL,
+    transactions BIGINT NOT NULL
 );
-
-CREATE TABLE ohlcv_data (
-    time TIMESTAMPTZ NOT NULL,
-    ticker TEXT REFERENCES stock_metadata(ticker),
-    open NUMERIC,
-    high NUMERIC,
-    low NUMERIC,
-    close NUMERIC,
-    volume BIGINT,
-    PRIMARY KEY (time, ticker)
-);
-SELECT create_hypertable('ohlcv_data', 'time');
 
